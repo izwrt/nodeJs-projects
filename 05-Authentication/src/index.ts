aac9f9662e156ee58caab795aa27033c0e9e36b7
izwrt <ishwart466@gmail.com>
2026-05-15 23:05:37 +0530

    Researching

--- 05-Authentication/src/index.ts
+++ 05-Authentication/src/index.ts
@@ -3,6 +3,8 @@ import express from "express";
 const app = express();
 const PORT = 8000
 
+app.use(express.json());
+
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
 });
