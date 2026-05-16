9ae17294ea335844a42d065209870d55263b094e
izwrt <ishwart466@gmail.com>
2026-05-16 23:13:53 +0530

    wip: partial authentication setup for sign-in route

--- 05-Authentication/src/index.ts
+++ 05-Authentication/src/index.ts
@@ -1,9 +1,15 @@
-import express from "express";
+import express from "express"
+import type {Request, Response} from "express"
 
 const app = express();
 const PORT = 8000
 
 app.use(express.json());
+
+// Sign up - basically collecting data from the user to authenticate in the future
+app.post("/sign-up", (req: Request, res: Response) => {
+  
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
