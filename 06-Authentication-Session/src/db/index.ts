6246605f4090e53bdcee0d61999393202ab5aa2b
izwrt <ishwart466@gmail.com>
2026-05-21 18:16:24 +0530

    feat: configure Docker Compose and Drizzle Kit

---
+++ 06-Authentication-Session/src/db/index.ts
@@ -0,0 +1,9 @@
+import { drizzle } from 'drizzle-orm/node-postgres';
+
+const connection = process.env.DATABASE_URL;
+if(!connection) {
+    throw new Error("Database URL is not set")
+}
+const db = drizzle(connection);
+
+export default db;
---
+++ 06-Authentication-Session/src/index.ts
@@ -0,0 +1,3 @@
+import 'dotenv/config';
+import db from './db/index.js';
+
