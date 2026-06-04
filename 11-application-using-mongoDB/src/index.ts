3b979274b278916499e5429816e7079626be6245
izwrt <ishwart466@gmail.com>
2026-06-04 18:11:53 +0530

    Refactor: enhance the database connection

--- 11-application-using-mongoDB/src/index.ts
+++ 11-application-using-mongoDB/src/index.ts
@@ -1,12 +1,21 @@
-import express from 'express';
+import express from "express";
 import "dotenv/config";
+import { connectMongoDB } from "./connection.js";
 
-const app =  express();
+const app = express();
 app.use(express.json());
-
 
 const PORT = process.env.PORT ?? 8000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on PORT ${PORT}`);
-})
+connectMongoDB(process.env.MONGODB_URL!)
+  .then(() => {
+    console.log(`MongoDB Connected`);
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on PORT ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB", err);
+    process.exit(1);
+  });
