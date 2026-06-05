b6afd3660b9143c483f19b0bd419664498696311
izwrt <ishwart466@gmail.com>
2026-06-05 17:18:07 +0530

    feat: create user schema and model using mongoose

---
+++ 11-application-using-mongoDB/src/models/user.model.ts
@@ -0,0 +1,21 @@
+import { Schema } from 'mongoose';
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    salt: {
+        type: String,
+        required: true
+    }
+})
